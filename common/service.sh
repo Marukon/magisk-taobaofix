636d543f562bb1a923c171e010bfb5f27388fed0
Maruko <zhxuhu@outlook.com>
2019-03-30 13:44:53 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -5,5 +5,12 @@
 # This will make sure your module will still work
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
-
+rm -rf /data/data/com.tmall.wireless/files/bundleBaseline/*; 
+rm -rf /data/data/com.taobao.taobao/files/bundleBaseline/*; 
+rm -rf /data/data/com.taobao.taobao/files/awcn_strategy/*; 
+rm -rf /data/data/com.tmall.wireless/files/awcn_strategy/*; 
+chmod 500 /data/data/com.tmall.wireless/files/bundleBaseline; 
+chmod 500 /data/data/com.taobao.taobao/files/bundleBaseline; 
+chmod 500 /data/data/com.tmall.wireless/files/awcn_strategy; 
+chmod 500 /data/data/com.taobao.taobao/files/awcn_strategy;
 # This script will be executed in late_start service mode
