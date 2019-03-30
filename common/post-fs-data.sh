1f9fe33564433e41104e119023b80cf099706d49
Maruko <zhxuhu@outlook.com>
2019-03-30 13:41:39 +0800

    1

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,7 +1,9 @@
 #!/system/bin/sh
-# 请不要硬编码 /magisk/modname/... ; 请使用 $MODDIR/...
-# 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-# 这个脚本将以 post-fs-data 模式执行
-# 更多信息请访问 Magisk 主题
+# This script will be executed in post-fs-data mode
